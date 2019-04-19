e0b26c88dc426fc899777e8442b2c1c0106b5b4a
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-04-19 09:39:06 -0700

    passport setup; enabling google oauth api

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-app.get('/', (req, res) => {
+passport.use(new GoogleStrategy({
 
-   res.send({ by: "buddy" });
-});
-
+}));
+// id
+"293055994795-c915pmk3bpsto5r2di77nh0onopp6027.apps.googleusercontent.com"
+// secret
+"fr3uavU95Jpa2AY8wWm0u8_J"
 const port = process.env.PORT || 5000;
 app.listen(port);
