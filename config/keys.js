510c428dded8ae50e07a1f9847ff62adadeac903
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-04-19 09:55:29 -0700

    securing api keys; google strategy options

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+   googleClientID: "293055994795-c915pmk3bpsto5r2di77nh0onopp6027.apps.googleusercontent.com",
+   googleClientSecret: "fr3uavU95Jpa2AY8wWm0u8_J"
+};
--- server/index.js
+++ server/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
 
 passport.use(new GoogleStrategy({
-
+   clientID: keys.googleClientID,
+   clientSecret: keys.googleClientSecret,
+   callbackURL: "/auth/google/callback"
+}, (accessToken) => {
+   console.log(accessToken);
 }));
-// id
-"293055994795-c915pmk3bpsto5r2di77nh0onopp6027.apps.googleusercontent.com"
-// secret
-"fr3uavU95Jpa2AY8wWm0u8_J"
+
 const port = process.env.PORT || 5000;
 app.listen(port);
