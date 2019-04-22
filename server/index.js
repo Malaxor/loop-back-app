2545bd2b4bd04203da22ac8bce8d26d73aa209d4
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-04-22 15:48:15 -0700

    server structure refactor

--- server/index.js
+++ server/index.js
@@ -1,21 +1,7 @@
+const port = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
+require('./routes/authRoutes')(app);
+require('./services/passport');
 
-passport.use(new GoogleStrategy({
-   clientID: keys.googleClientID,
-   clientSecret: keys.googleClientSecret,
-   callbackURL: "/auth/google/callback"
-}, (accessToken, refreshToken, profile, done) => {
-   console.log({ accessToken, refreshToken, profile });
-}));
-
-app.get('/auth/google', passport.authenticate('google', { 
-   scope: ['profile', 'email']
-}));
-app.get('/auth/google/callback', passport.authenticate('google'));
-
-const port = process.env.PORT || 5000;
 app.listen(port);
