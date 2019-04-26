3348a53687ac96c9c9c7fec1273a61d7792c6153
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-04-26 16:27:01 -0700

    mongoose queries; encoding users

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,11 +5,25 @@ const User = mongoose.model('users');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 
+passport.serializeUser((user, done) => {
+   done(null, user.id);
+});
 
 passport.use(new GoogleStrategy({
    clientID: keys.googleClientID,
    clientSecret: keys.googleClientSecret,
    callbackURL: "/auth/google/callback"
 }, (accessToken, refreshToken, profile, done) => {
-   new User({ googleId: profile.id }).save();
+
+   User.findOne({ googleId: profile.id })
+   .then(foundUser => {
+      if(foundUser) {
+         done(null, foundUser);
+      }
+      else {
+         new User({ googleId: profile.id })
+         .save()
+         .then(newUser => done(null, newUser));
+      }
+   });
 }));
