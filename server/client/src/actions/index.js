35dddcfeca0907661a87c36ecfa63f14efbfda86
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-05-08 17:54:59 -0700

    refactoring the App; testing fetchUser action creator

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 import { FETCH_USER } from './types'
 
-const fetchUser = () => {
-   axios.get('/api/user');
+export const fetchUser = () => async dispatch => {
+   
+   const currentUser = await axios.get('/api/user');
+   dispatch({
+      type: FETCH_USER,
+      payload: currentUser
+   });
 }
--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+// importing everything from the actions folder, then assigning everything to the actions variable object
+import * as actions from '../actions';
 
 import Header from './Header';
 
@@ -7,14 +10,24 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveryNew = () => <h2>SurveryNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => (
-   <BrowserRouter>
-      <div className="container">
-         <Header />
-         <Route exact path='/survey' component={Dashboard} />
-         <Route exact path='/survey/new' component={SurveryNew} />
-         <Route exact path='/landing' component={Landing} />
-      </div>
-   </BrowserRouter>
-);
-export default App;
+class App extends Component {
+
+   componentDidMount() {
+      this.props.fetchUser();
+   }
+   render() {
+      return (
+         <div className='container'>
+            <BrowserRouter>
+               <div>
+                  <Header />
+                  <Route exact path='/survey' component={Dashboard} />
+                  <Route exact path='/survey/new' component={SurveryNew} />
+                  <Route exact path='/landing' component={Landing} />
+               </div>
+            </BrowserRouter>
+         </div>
+      );
+   }
+}
+export default connect(null, actions)(App);
