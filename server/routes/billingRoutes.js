2303d4a4e19ebe59f04c164a6ac8f0eb984151da
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-05-13 17:40:48 -0700

    requiring authentication

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -3,15 +3,18 @@ const stripe = require('stripe')(stripeSecretKey);
 
 module.exports = app => {
    
-   app.post('/api/stripe', async(res, req) => {
-
+   app.post('/api/stripe', async(req, res) => {
+      // if someone attempts to add credits without being logged in
+      if(!req.user) {
+         return res.status(401).send({ error: 'You must log in' });
+      }
       const charge = await stripe.charges.create({
          amount: 500,
          currency: 'usd',
          description: '$5 for five credits',
-         source: body.req.id
+         source: req.body.id
       });
-      
+
       req.user.credits += 5;
       const user = await req.user.save();
       res.send(user);
