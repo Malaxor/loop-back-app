3079e9ea83c3c96cf95821f1154327cb9ac6978a
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-05-14 17:43:33 -0700

    routing in production

--- server/index.js
+++ server/index.js
@@ -24,4 +24,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+/********************** Heroku Production Setup ****************************/
+
+// having Express handle routes that are located inside the client folder
+if(process.env.NODE_ENV === 'production') {
+   // express will serve production assets (main.js or main.css)
+   app.use(express.static('client/build'));
+   // express will serve index.html if it doesn't recognize the route
+   const path = require('path');
+   // last resort for responding to a route request that isn't located in any other folder
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   });
+}  
 app.listen(port);
