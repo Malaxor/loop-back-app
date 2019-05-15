0e45e302be21efb18b8a58a145b45102a04c9e3e
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-05-14 18:48:06 -0700

    added billing and client-side app

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const port = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
+const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
@@ -25,16 +26,15 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 /********************** Heroku Production Setup ****************************/
-
 // having Express handle routes that are located inside the client folder
 if(process.env.NODE_ENV === 'production') {
    // express will serve production assets (main.js or main.css)
    app.use(express.static('client/build'));
    // express will serve index.html if it doesn't recognize the route
-   const path = require('path');
    // last resort for responding to a route request that isn't located in any other folder
    app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
    });
-}  
+}
+/************************************************************************/
 app.listen(port);
