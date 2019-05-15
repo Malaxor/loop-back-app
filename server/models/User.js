c0c87349dc61207daad9178c334d908bef9d07d8
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-05-15 16:08:22 -0700

    added user relationship field to the surveySchema

--- server/models/User.js
+++ server/models/User.js
@@ -9,7 +9,7 @@ const userSchema = new Schema({
    }
 });
 // loading a model into mongo via mongoose: one argument signifies extraction; two arguments signify the creation of a class
-mongoose.model('users', userSchema);
+mongoose.model('User', userSchema);
 
 // Important Note: we don't export this file via module.exports in a testing environment
 // because the model files will be required into the project multiple times, thus confusing
