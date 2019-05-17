54b4aa0f59c6b79f26e049d57b1c51cf530c73df
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-05-16 18:37:43 -0700

    improving the email template; polishing the route handle

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import axios from 'axios';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import reducers from './reducers';
 import App from './components/App';
+
+window.axios = axios;
 
 const store = createStore(reducers, applyMiddleware(reduxThunk));
 
--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -2,16 +2,15 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Survey = mongoose.model('Survey');
-// Sendgrid Mailer constructor
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 module.exports = app => {
    // middleware will get called in the order specified (left to right)
-   app.get('/api/surveys', requireLogin, requireCredits, (req, res) => {
-
+   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+      // survey input
       const { title, subject, body, recipients } = req.body;
-
+      // new survey model
       const survey = new Survey({
          title,
          subject,
@@ -21,5 +20,16 @@ module.exports = app => {
          dateSent: Date.now()
       });
       const mailer = new Mailer(survey, surveyTemplate(survey));
+      try {
+         await mailer.send();
+         await survey.save();
+   
+         req.user.credits -= 1;
+         const user = await req.user.save();
+         res.send(user);
+      } 
+      catch (err) {
+         res.status(422).send(err);
+      }
    });
 }
