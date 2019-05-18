af0a7f6a5eeb4ac6365ed4d9ec1899410aaa1d73
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-05-17 17:23:07 -0700

    survey form working

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import _ from 'lodash';
+import SurveyField from './SurveyField';
 
-class SurveyNew extends Component {
-   
+const FIELDS = [
+   { label: 'Survey Title', name: 'title' },
+   { label: 'Subject Line', name: 'subject' },
+   { label: 'Email Body', name: 'body' },
+   { label: 'Recipient List', name: 'emails' }
+];
+
+class SurveyForm extends Component {
+
+   renderFields() {
+      return _.map(FIELDS, ({ label, name }) => {
+         return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+      });   
+   }
    render() {
       return (
 
          <div>
-            SurveyNew
+            <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+               {this.renderFields()}
+               <button type='submit'>Submit</button>
+            </form>   
          </div>
       );
    }
 }
-export default SurveyNew;
+export default reduxForm({
+   form: 'surveyForm'
+ })(SurveyForm);
