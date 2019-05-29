686cc2d01d9e8eb7fb99c6a8fe252a6a4f3ff416
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-05-29 16:57:01 -0700

    lodash chain helper

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -35,12 +35,10 @@ module.exports = app => {
    });
    app.post('/api/surveys/webhooks', (req, res) => {
 
-      const events = _.map(req.body, ({ url, email }) => {
-
-         const pathname = new URL(url).pathname;
-         const p = new Path(`/api/surveys/:surveyId/:choice`);
-         const match = p.test(pathname);
-
+      const p = new Path(`/api/surveys/:surveyId/:choice`);
+      
+      const events = _.chain(req.body).map(({ url, email }) => {
+         const match = p.test(new URL(url).pathname);
          if(match) {
             return {
                email,
@@ -48,9 +46,10 @@ module.exports = app => {
                choice: match.choice
             };
          }
-      });
-      const compactEvents = _.compact(events);
-      const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
-      console.log(uniqueEvents);
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value();
+      console.log(events);
    });
 }
